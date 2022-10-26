e11fa44d6efb7a29effdcfd77e993ee1786b2de3
bareb0nes <molocmola@gmail.com>
2022-10-26 18:49:34 +0200

    Adding existing project files

---
+++ assets/backgrounds/puzzle 1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="puzzle 1" tilewidth="8" tileheight="8" tilecount="3328" columns="64">
+ <image source="puzzle 1.png" width="512" height="416"/>
+</tileset>
